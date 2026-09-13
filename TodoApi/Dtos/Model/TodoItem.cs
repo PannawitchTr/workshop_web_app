54b45e57b98cbb4a5670377e3e157034cd21bc51
Pannawitch Trathongkum <pannwitch.t@ku.th>
2026-09-13 13:12:23 +0700

    [dev] add TodoItem data model

---
+++ TodoApi/Dtos/Model/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace TdodoApi.Models;
+
+public class TodoItem
+{
+    public int MyProperty { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
